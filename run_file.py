bad2426ce884c539f8fe5ae13d54f109945f8ee0
Ian B <44586707+ibiggy9@users.noreply.github.com>
2023-09-25 16:01:45 -0400

    Update run_file.py

--- run_file.py
+++ run_file.py
@@ -1,10 +1,20 @@
+# Importing required modules
 import schedule
 import os
 
 def run():
-    os.system("python3.9 /Users/main/desktop/code/scraping/dare_stuff/new_scrapers/search_performance/inputs.py")
+    """
+    Execute the target Python script and print a message to indicate execution.
+    """
+    # Run the Python script using os.system
+    os.system("python3.9 inputs.py")
+    
+    # Print message to console to indicate that the script is running
     print('running')
+
+# Schedule the 'run' function to execute every 24 hours
 schedule.every(24).hours.do(run)
 
+# Keep the script running and check for pending scheduled tasks
 while True:
     schedule.run_pending()
